2b666aeed86eceb8c16c074d3821a383ee8f46ef
projects-gustavo <gustavo.martins145@etec.sp.gov.br>
2023-01-17 15:17:00 -0300

    Alteração no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import {
     RiInstagramFill,
     RiLinkedinFill,
     RiGithubFill,
+    RiWhatsappFill
 } from 'react-icons/ri'
 import ScrollToTop from 'react-scroll-to-top'
 
@@ -11,21 +12,25 @@ const Footer = () => {
         <footer className="bg-backgroundSecondary p-8 xl:p-20 mt-12" id="contact">
             <ScrollToTop smooth top="20" className="flex justify-center items-center bg-gradient-to-r from-primary to-secondary rounded-full 
             shadow-none" color="#000" width="20px" height="20px" />
-            <p className="text-center text-white mb-3 font-medium">Redes Sociais</p>
+            <p className="text-center text-white mb-3 font-medium">Entre em contato</p>
             <div className="flex items-center justify-center border-b border-neutral-800 pb-8">
                 <nav className="flex items-center gap-4 bg-backgroundSecondary">
+                    <a href="https://wa.me/5511986319802" className="block p-4 rounded-xl text-white bg-backgroundPrimary 
+                    hover:bg-gradient-to-r from-primary to-secondary hover:text-black duration-300" target="_blank">
+                        <RiWhatsappFill className="scale-150" />
+                    </a>
                     <a href="https://www.linkedin.com/in/guhrodrigues/" className="block p-4 rounded-xl text-white bg-backgroundPrimary 
                     hover:bg-gradient-to-r from-primary to-secondary hover:text-black duration-300" target="_blank">
                         <RiLinkedinFill className="scale-150" />
                     </a>
                     <a href="https://github.com/projects-gustavo" className="block p-4 rounded-xl bg-backgroundPrimary text-white 
-                    hover:bg-gradient-to-r from-primary to-secondary hover:text-black duration-300" 
-                    target="_blank">
+                    hover:bg-gradient-to-r from-primary to-secondary hover:text-black duration-300"
+                        target="_blank">
                         <RiGithubFill className="scale-150" />
                     </a>
                     <a href="https://www.instagram.com/gustavinrm/" className="block p-4 rounded-xl bg-backgroundPrimary text-white 
-                    hover:bg-gradient-to-r from-primary to-secondary hover:text-black duration-300" 
-                    target="_blank">
+                    hover:bg-gradient-to-r from-primary to-secondary hover:text-black duration-300"
+                        target="_blank">
                         <RiInstagramFill className="scale-150" />
                     </a>
                 </nav>
