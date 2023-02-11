2d3093ee08faa9975d75ae92dba07dca9956869a
projects-gustavo <gustavo.martins145@etec.sp.gov.br>
2023-02-11 19:01:21 -0300

    Alterações

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 import { RiDownload2Fill } from 'react-icons/ri'
 
 // Assets
-import about from '/assets/img/about.jpg'
-import curriculo from '/assets/docs/curriculo.pdf'
+import about from '../../public/assets/img/about.jpg'
+import curriculo from '../../public/assets/docs/curriculo.pdf'
 
 // About
 const About = () => {
