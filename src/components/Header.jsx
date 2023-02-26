5ea4a228baedf50fe80ef355111bf4e72da4c54d
projects-gustavo <gustavo.martins145@etec.sp.gov.br>
2023-02-26 17:26:38 -0300

    Ajustes 🚀

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,11 +13,15 @@ const Header = () => {
             <div className="xl:w-1/3 2xl:w-[35.7%] text-center 2xl:mr-12">
                 <a
                     href="#home"
-                    className="sm:text-[1.8rem] lg:text-3xl xl:text-4xl 2xl:text-[2.5rem] font-signature font-semibold relative p-2 text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
+                    className="sm:text-[1.8rem] lg:text-3xl xl:text-4xl 2xl:text-[2.5rem] font-signature font-semibold relative p-2 text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary"
+                >
                     Gustavo
                 </a>
             </div>
-            <nav className={`fixed w-full md:w-[40%] xl:w-full h-full ${showMenu ? "left-0 top-[3.5rem] bg-backgroundPrimary/90" : "-left-full top-[3.5rem]"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50 xl:ml-12`}>
+            <nav
+                className={`fixed w-full md:w-[40%] xl:w-full h-full ${showMenu ? "left-0 bg-backgroundPrimary/90" : "-left-full"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50 xl:ml-12`}
+                style={showMenu ? {top: '9vh'} : {top: '9vh'}}
+            >
                 <li onClick={closeMobileIcon}>
                     <a href="#home">Início</a>
                 </li>
