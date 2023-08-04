744ee825237169f600327b5b802c926c5581381d
guhrodriguess <gumartinsR1201@gmail.com>
2023-08-04 12:43:06 -0300

    feat: adding new personal logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,41 @@
 import { useContext, useEffect } from "react";
 import AppContext from "@/contexts/AppContext";
 
+import { Link } from "react-router-dom";
+
 import LinksMenuNav from "./LinksMenuNav.tsx";
 import MobileButton from "./MobileButton";
 import ContactButton from "./ContactButton";
+
+import logo from "@/assets/images/logo.svg";
 
 export default function Header() {
     const { pageScrolled, setPageScrolled } = useContext(AppContext);
 
     useEffect(() => {
         const handlePageScrolled = () => {
-            if (window.scrollY >= 90) {
+            if (window.scrollY > 20) {
                 setPageScrolled(true);
             } else {
                 setPageScrolled(false);
             }
         };
+
         window.addEventListener("scroll", handlePageScrolled);
     }, []);
 
     return (
         <header
-            className={`flex w-full h-[70px] z-50 fixed sm:bg-background md:backdrop-blur-[5px] ${
+            className={`flex w-full h-[70px] z-50 fixed top-0 sm:bg-background md:border-b ${
                 pageScrolled
-                    ? "md:border-b border-neutral-800 md:bg-background/60"
-                    : undefined
+                    ? "md:border-neutral-800 md:bg-background/60 md:backdrop-blur-[5px]"
+                    : "border-transparent"
             }`}
         >
             <nav className="max-w-5xl w-full mx-auto px-6 justify-between top-0 flex items-center gap-10">
-                <div className="text-center">
-                    <h1 className="text-lg font-semibold relative text-primary">
-                        Gustavo
-                    </h1>
-                </div>
+                <Link to="/">
+                    <img src={logo} width={25} alt="Logo" />
+                </Link>
 
                 <LinksMenuNav />
 
