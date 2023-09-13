ec979917d9b46ccd0d9fc0edb23a6647e26658d8
guhrodriguess <gumartinsR1201@gmail.com>
2023-09-13 17:51:09 -0300

    refactor: Adding utils

---
+++ src/utils/index.tsx
@@ -0,0 +1,3 @@
+export const emailService = import.meta.env.VITE_EMAIL_SERVICE;
+export const emailTemplate = import.meta.env.VITE_EMAIL_TEMPLATE;
+export const publicKey = import.meta.env.VITE_PUBLIC_KEY;
