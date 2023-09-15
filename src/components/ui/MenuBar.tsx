45cc755e62c07c525da883eb8217754a2d33e277
guhrodriguess <gumartinsR1201@gmail.com>
2023-09-15 19:34:27 -0300

    feat: Adding animation to the menubar

--- src/components/ui/MenuBar.tsx
+++ src/components/ui/MenuBar.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
 import { useHooks } from "@/hooks/Provider";
+import { useMenuAnimation } from "@/hooks/menuAnimation";
 
 import { navLinks } from "@/data/header";
 
 export function MenuBar() {
   const { showMenu, setShowMenu } = useHooks();
+
+  const scope = useMenuAnimation(showMenu);
 
   const handleCloseMenu = () => {
     setShowMenu(false);
@@ -14,24 +17,31 @@ export function MenuBar() {
 
   return (
     <div
-      className={`fixed right-0 z-50 w-32 mx-6 p-1 my-[70px] flex flex-col bg-background border border-neutral-800 rounded-lg duration-150 ${
+      className={`fixed right-0 z-50 w-32 mx-6 p-1 my-[70px] flex flex-col bg-background border border-neutral-800 rounded-lg duration-300 ${
         showMenu
           ? "top-0 visible opacity-100 scale-100"
-          : "invisible opacity-0 scale-90"
+          : "invisible opacity-0 scale-50"
       }`}
+      ref={scope}
     >
-      {navLinks.map(({ id, path, name, icon }) => (
-        <Link
-          key={id}
-          to={path}
-          className="px-2 py-2.5 flex items-center gap-4 text-sm text-primary"
-          onClick={handleCloseMenu}
-        >
-          <span>{icon}</span>
+      <ul
+        style={{
+          pointerEvents: showMenu ? "auto" : "none",
+        }}
+      >
+        {navLinks.map(({ id, path, name, icon }) => (
+          <Link
+            key={id}
+            to={path}
+            className="px-2 py-2.5 flex items-center gap-4 text-sm text-primary"
+            onClick={handleCloseMenu}
+          >
+            <span>{icon}</span>
 
-          {name}
-        </Link>
-      ))}
+            {name}
+          </Link>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/hooks/menuAnimation.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+
+import { useAnimate, stagger } from "framer-motion";
+
+export function useMenuAnimation(showMenu: boolean) {
+  const [scope, animate] = useAnimate();
+
+  const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+
+  useEffect(() => {
+    animate(
+      "ul",
+      {
+        clipPath: showMenu
+          ? "inset(0% 0% 0% 0% round 10px)"
+          : "inset(10% 50% 90% 50% round 10px)",
+      },
+      {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5,
+      }
+    );
+
+    animate(
+      "a",
+      showMenu
+        ? { opacity: 1, scale: 1, filter: "blur(0px)" }
+        : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
+      {
+        duration: 0.2,
+        delay: showMenu ? staggerMenuItems : 0,
+      }
+    );
+  }, [showMenu]);
+
+  return scope;
+}
