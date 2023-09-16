a05e5f01c0f6f20d2833a3ce3214c470d015e23e
guhrodriguess <gumartinsR1201@gmail.com>
2023-09-16 14:13:24 -0300

    feat: Updating footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react";
+
 import { socials } from "@/data/socials";
 
 import { Button } from "@/components/utils/Button";
 import { Typography } from "@/components/utils/Typography";
 
 export function Footer() {
+  const [date, setDate] = useState<Date>(new Date());
+
+  function refreshClock() {
+    setDate(new Date());
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
-    <footer className="max-w-5xl w-full mx-auto p-6">
-      <div className="flex items-center sm:justify-center md:justify-between">
+    <footer className="max-w-5xl w-full mx-auto h-20">
+      <div className="flex items-center sm:justify-center md:justify-between h-full px-6">
         <div className="flex gap-3">
           {socials.map(({ id, icon, href }) => (
             <a key={id} href={href} target="_blank">
@@ -17,7 +33,11 @@ export function Footer() {
           ))}
         </div>
         <Typography variant="muted" size="sm" className="sm:hidden md:block">
-          {new Date().getFullYear()} © Gustavo Rodrigues
+          {date.toLocaleString("pt-BR", {
+            hour: "2-digit",
+            minute: "numeric",
+            second: "numeric",
+          })}
         </Typography>
       </div>
     </footer>
