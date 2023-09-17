abd72110d19e876d0ebb24e4e59c313fb71e375e
guhrodriguess <gumartinsR1201@gmail.com>
2023-09-16 21:04:51 -0300

    style: Updating Tooltip

--- src/components/utils/Tooltip.tsx
+++ src/components/utils/Tooltip.tsx
@@ -12,7 +12,7 @@ export function Tooltip({ children, text }: TooltipProps) {
       className="relative select-none"
     >
       <div
-        className={`w-[83px] bg-background border border-neutral-800 text-foreground font-medium rounded-lg text-xs py-1.5 px-2 -right-3 lg:-right-4 -top-10 absolute duration-150 ${
+        className={`-top-9 absolute w-[83px] bg-background border border-neutral-800 text-foreground font-medium rounded-lg text-xs py-1.5 px-2 -right-3 lg:-right-4 lg:-translate-x-0.5 duration-150 ${
           isVisible
             ? "visible opacity-100 scale-100"
             : "invisible opacity-0 scale-50"
