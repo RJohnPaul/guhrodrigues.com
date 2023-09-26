795c8a0d0d0dfbd2c98f81cd87fa1b4f548ca63d
guhrodriguess <gumartinsR1201@gmail.com>
2023-09-25 22:36:13 -0300

    style: Updating ProjectItem

--- components/ui/ProjectItem.tsx
+++ components/ui/ProjectItem.tsx
@@ -41,7 +41,7 @@ export function ProjectItem({
       onHoverEnd={() => setHovered('')}
       className="cursor-pointer select-auto"
     >
-      <section className="relative flex flex-col rounded-xl">
+      <section className="relative flex flex-col rounded-xl px-7">
         {isHovered && (
           <motion.div
             className="absolute rounded-xl bg-neutral-800 inset-0 z-[-1]"
@@ -51,19 +51,22 @@ export function ProjectItem({
             exit={{ opacity: 0 }}
           />
         )}
-        <figure className="overflow-hidden rounded-t-xl px-2 mt-4">
+        <figure className="overflow-hidden rounded-t-xl mt-4">
           <Image
             src={src}
-            className="max-w-[420px] w-full md:mx-auto max-h-[280px] h-full object-cover rounded-xl"
+            className="max-w-[400px] w-full md:mx-auto max-h-[200px] h-full object-cover rounded-xl"
             alt={title}
             placeholder="blur"
             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAIAAAAmkwkpAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAP0lEQVQImQE0AMv/AFBQUJKSkqmpqaOjowCurq7v7+/Jycm5ubkA////jIyMn5+fg4ODADAwMD09PWlpaQAAAApRGnEHblMWAAAAAElFTkSuQmCC"
           />
         </figure>
-        <div className="flex flex-col w-full py-4 px-5 space-y-4">
+        <div className="flex flex-col w-full py-4 space-y-4">
           <div className="flex flex-col justify-center space-y-2">
             <h3 className="font-bold text-xl text-primary">{title}</h3>
-            <Typography size="sm" className="max-w-[420px] w-full">
+            <Typography
+              size="sm"
+              className="max-w-[420px] w-full leading-loose"
+            >
               {description}
             </Typography>
           </div>
