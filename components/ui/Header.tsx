85aa3735e266092dbc203921a588f9b2dad19d6d
guhrodriguess <gumartinsR1201@gmail.com>
2023-10-03 21:24:37 -0300

    style: Adding ping image header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -16,12 +16,17 @@ export function Header() {
   return (
     <header className="sticky top-0 z-10 h-20 lg:h-auto flex self-start lg:flex-col w-full lg:max-w-[230px] lg:w-1/2 bg-background lg:bg-transparent">
       <div className="flex items-center lg:items-start gap-2 lg:gap-0.5 lg:flex-col w-full">
-        <div className="group p-2 lg:p-5 my-2 rounded-full overflow-hidden border bg-background border-neutral-800">
-          <Image
-            src={logo}
-            alt="Gustavo"
-            className="w-5 lg:w-10 scale-100 group-hover:scale-105 ease-in-out duration-300"
-          />
+        <div className="group relative p-2 lg:p-5 my-2 rounded-full border bg-background border-neutral-800">
+          <div className="overflow-hidden">
+            <Image
+              src={logo}
+              alt="Gustavo"
+              className="w-5 lg:w-10 scale-100 group-hover:scale-105 ease-in-out duration-300"
+            />
+          </div>
+          <div className="hidden lg:block absolute w-4 h-4 rounded-full bg-emerald-600 bottom-1 right-1">
+            <div className="absolute z-[11] w-4 h-4 rounded-full bg-emerald-600 bottom-0 -right-0 animate-ping transition-[4s]" />
+          </div>
         </div>
         <div>
           <Title variant="title" size="xl" className="hidden md:block">
