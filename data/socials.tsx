d13e65a392d4848737d37dfd37741f317294c89a
guhrodriguess <gumartinsR1201@gmail.com>
2023-10-11 12:33:11 -0300

    refactor: Updating eslint

--- data/socials.tsx
+++ data/socials.tsx
@@ -1,24 +1,24 @@
-import { Github, Linkedin, Instagram } from "lucide-react";
+import { Github, Linkedin, Instagram } from 'lucide-react'
 
-import { SocialsProps } from "@/types";
+import { SocialsProps } from '@/types'
 
 export const socials: SocialsProps[] = [
   {
     id: 1,
-    href: "https://github.com/guhrodriguess",
-    name: "GitHub",
+    href: 'https://github.com/guhrodriguess',
+    name: 'GitHub',
     icon: <Github size={16} />,
   },
   {
     id: 2,
-    href: "https://www.linkedin.com/in/guhrodrigues",
-    name: "LinkedIn",
+    href: 'https://www.linkedin.com/in/guhrodrigues',
+    name: 'LinkedIn',
     icon: <Linkedin size={16} />,
   },
   {
     id: 3,
-    href: "https://www.instagram.com/guhrodrrigues",
-    name: "Instagram",
+    href: 'https://www.instagram.com/guhrodrrigues',
+    name: 'Instagram',
     icon: <Instagram size={16} />,
   },
-];
+]
