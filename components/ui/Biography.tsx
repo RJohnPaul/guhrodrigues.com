efd7c49c2f352b7fb7c9e484a43b2492ef9e14c0
guhrodriguess <gumartinsR1201@gmail.com>
2023-10-12 20:40:51 -0300

    feat: Updating biography

--- components/ui/Biography.tsx
+++ components/ui/Biography.tsx
@@ -3,12 +3,12 @@ export function Biography() {
     <div className="leading-relaxed space-y-3">
       <p className="text-md text-foreground">
         Meu nome é Gustavo Rodrigues, sou Desenvolvedor Web e estudante de
-        Técnico em Informática para Internet .
+        Técnico em Informática para Internet.
       </p>
       <p className="text-md text-foreground">
         Comecei em 2021 na programação com HTML, CSS e JavaScript. No ano
-        seguinte, minha paixão pela programação só cresceu, e ficou claro que
-        essa seria minha profissão definitiva.
+        seguinte, minha paixão só cresceu e ficou claro que essa seria minha
+        profissão definitiva.
       </p>
       <p className="text-md text-foreground">
         Adoro aprender novas tecnologias desde que descobri a programação.
@@ -16,8 +16,8 @@ export function Biography() {
         Node.js e JavaScript.
       </p>
       <p className="text-md text-foreground font-normal">
-        Interfaces minimalistas , dark mode e a tecnologia me encantam.
-        Desfrutar de músicas é uma terapia para mim.
+        Interfaces minimalistas, dark mode e a tecnologia me encantam. Desfrutar
+        de músicas é uma terapia para mim.
       </p>
     </div>
   )
