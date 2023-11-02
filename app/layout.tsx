0f69a80560151f80038214d49631e2c9e38f21b8
guhrodriguess <gumartinsR1201@gmail.com>
2023-11-02 11:14:20 -0300

    style: Updating blur

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "@/styles/globals.css";
 import { Provider } from "@/context/Provider";
 
 import { Texture } from "@/components/ui/Texture";
+import { Blur } from "@/components/ui/Blur";
 import { AnimateEnter } from "@/components/utils/AnimateEnter";
 import { Sidebar } from "@/components/sidebar/Sidebar";
 
@@ -94,6 +95,7 @@ export default function RootLayout({
           </Provider>
         </AnimateEnter>
         <Texture />
+        <Blur />
       </body>
     </html>
   );
---
+++ components/ui/Blur.tsx
@@ -0,0 +1,5 @@
+export function Blur() {
+  return (
+    <div className="absolute -top-[450px] right-[400px] z-[-999] h-[250px] w-[900px] rounded-full bg-emerald-400 blur-[350px] max-lg:hidden" />
+  );
+}
