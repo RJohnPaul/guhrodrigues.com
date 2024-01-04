84587ed23d97cf84bdc64ce9c78184b30c6497f7
guhrodriguess <gumartinsR1201@gmail.com>
2024-01-04 20:05:18 -0300

    style: Updating tooltip

--- components/elements/Tooltip.tsx
+++ components/elements/Tooltip.tsx
@@ -20,7 +20,7 @@ const Tooltip = (props: TooltipProps) => {
     >
       <div
         className={cn(
-          "border-border bg-muted absolute -bottom-9 right-0 flex w-[7.0rem] items-center justify-center rounded-lg border px-1.5 py-1 text-xs text-primary/60 duration-150",
+          "absolute -bottom-9 right-0 flex w-[7.0rem] items-center justify-center rounded-lg border border-border bg-muted px-1.5 py-1 text-xs text-primary/60 duration-150",
           {
             "visible scale-100 opacity-100": isToastVisible,
             "invisible scale-50 opacity-0": !isToastVisible,
